dd7b9e2d1c70ad942747d24da48b2d1108fec9ce
EcoSensors <65593795+ecosensors@users.noreply.github.com>
2022-08-18 14:07:21 +0200

    Update

--- EcoSensors/console/map/serializers.py
+++ EcoSensors/console/map/serializers.py
@@ -11,7 +11,7 @@ from .models import Stations
 
 class StationsSerializer(serializers.GeoFeatureModelSerializer):
     class Meta:
-        fields = ("id_station", "station_name", "station_longname", "station_lat", "station_lng")
+        fields = ("id_station", "station_name", "station_longname", "station_lat", "station_lng", "fields_id_field")
         geo_field = "location"
         model = Stations
 
--- EcoSensors/console/map/viewsets.py
+++ EcoSensors/console/map/viewsets.py
@@ -1,6 +1,7 @@
 """Markers API views."""
 # Maps with Django (2)
 # https://www.paulox.net/2021/07/19/maps-with-django-part-2-geodjango-postgis-and-leaflet/
+#import self
 from rest_framework import viewsets
 from rest_framework_gis import filters
 
@@ -10,7 +11,16 @@ from map.serializers import StationsSerializer
 
 class MarkerViewSet(viewsets.ReadOnlyModelViewSet):
     """Marker view set."""
+    #print(self.kwargs.get('idfield'))
     bbox_filter_field = "location"
     filter_backends = (filters.InBBoxFilter,)
     queryset = Stations.objects.filter(station_active=1, map=1)
     serializer_class = StationsSerializer
+
+    def get_queryset(self, **kwargs):
+        #field_id = self.kwargs['idfield']
+        print(self.kwargs)
+        print(kwargs)
+        return Stations.objects.filter(fields_id_field=1)
+
+
