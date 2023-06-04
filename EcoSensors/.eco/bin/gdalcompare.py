a59822172f20a2b13cf8c280c5e7c3755b306064
EcoSensors <65593795+ecosensors@users.noreply.github.com>
2023-06-04 17:43:27 +0200

    Delete EcoSensors directory

--- EcoSensors/.eco/bin/gdalcompare.py
+++
@@ -1,4 +0,0 @@
-#!/Users/pamey/Documents/Django/EcoSensors/.eco/bin/python3
-# EASY-INSTALL-SCRIPT: 'GDAL==3.5.1','gdalcompare.py'
-__requires__ = 'GDAL==3.5.1'
-__import__('pkg_resources').run_script('GDAL==3.5.1', 'gdalcompare.py')
